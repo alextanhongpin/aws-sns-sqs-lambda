aeae09c124ed33fc9d34ff63c32463892c6f1e46
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-07-20 18:43:11 +0800

    Update working solution

--- consumer/handler.js
+++ consumer/handler.js
@@ -1,22 +1,33 @@
 'use strict'
 
 const AWS = require('aws-sdk')
-const sqs = new AWS.SQS()
+AWS.config.update({
+  region: 'ap-southeast-1',
+  credentials: new AWS.SharedIniFileCredentials({ profile: 'poc-delivery' })
+})
+const sqs = new AWS.SQS({
+  apiVersion: '2012-11-05'
+})
 
 // From sns-pub > sqs -> lambda
-module.exports.hello = (event, context, callback) => {
-  const sqsQueueUrl = 'https://sqs.eu-west-1.amazonaws.com/XXXXXXXXXXXX/dummy-queue'
+module.exports.consumer = (event, context, callback) => {
+  const sqsQueueUrl = ''
   const receiveMessageParams = {
     QueueUrl: sqsQueueUrl
   }
 
   sqs.receiveMessage(receiveMessageParams, (err, data) => {
-    if (data.Messages && data.Messages.length > 0) {
+    if (err) {
+      console.log(err)
+      return callback(err)
+    }
+    if (data && data.Messages && data.Messages.length > 0) {
           // Do something with data here
       var deleteMessageParams = {
         QueueUrl: sqsQueueUrl,
         ReceiptHandle: data.Messages[0].ReceiptHandle
       }
+      console.log(data)
       sqs.deleteMessage(deleteMessageParams, callback)
     }
   })
--- producer/handler.js
+++ producer/handler.js
@@ -1,17 +1,22 @@
 'use strict'
 
 const AWS = require('aws-sdk')
+AWS.config.apiVersions = {
+  sns: '2010-03-31'
+}
+AWS.config.region = 'ap-southeast-1'
+
 const sns = new AWS.SNS()
 
-module.exports.hello = (event, context, callback) => {
-  const eventText = JSON.stringify(event)
-
+module.exports.producer = (event, context, callback) => {
   const params = {
-    Message: eventText,
+    Message: JSON.stringify({
+      message: 'Hello world'
+    }),
     Subject: 'Text SNS from Lambda',
-    TopicArn: 'arn:aws:sns:ap-southeast-1:*:test-topic1'
+    TopicArn: 'arn:aws:sns:ap-southeast-1:*:*'
   }
 
-  // Publish to an sns
+  console.log('Publishing to SNS...')
   sns.publish(params, callback)
 }
